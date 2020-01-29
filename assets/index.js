34491aefabc8bca5a0dc2c2feb15c1283438efbf
Dainer Acosta <dainerudea@gmail.com>
2020-01-29 16:22:17 -0500

    Upload proyecto a GitHub

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({ 
+        el: video, 
+        plugins: [new AutoPlay(), new AutoPause()] 
+    });
+
+
+const buttonPlay = document.getElementById('playButton');   
+buttonPlay.onclick = () => player.togglePlay();
+
+const buttonMute = document.getElementById('muteButton');   
+buttonMute.onclick = () => player.toggleMute();
